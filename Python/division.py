ae86b6f3a1b644062c9f97249009e83b1173c9a0
sai <sai@team.quadloops.com>
2015-06-28 00:48:34 +0530

    Solution for HackerRank python division problem

---
+++ Python/division.py
@@ -0,0 +1,12 @@
+# Solution for HackerRank python division problem  
+
+__autor__ = 'Saimadhu Polamuri'
+__website__ = 'www.dataaspirant.com'
+__createdon__ = '28-Jun-2015'
+
+from __future__ import division
+number1 = int(raw_input())
+number2 = int(raw_input())
+
+print number1 // number2
+print number1 / number2
