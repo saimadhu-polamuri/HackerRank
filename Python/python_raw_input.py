b784cb24bbf468627d33333923e320cd098643c3
sai <sai@team.quadloops.com>
2015-06-28 00:35:03 +0530

    Solution for HackerRank python raw input problem

---
+++ Python/python_raw_input.py
@@ -0,0 +1,8 @@
+# Solution for HackerRank python raw input problem  
+
+__autor__ = 'Saimadhu Polamuri'
+__website__ = 'www.dataaspirant.com'
+__createdon__ = '28-Jun-2015'
+string = raw_input()
+if len(string):
+	print string
