2ab0ea2a0083061c659482ad862b89b5cd551d0c
sai <sai@team.quadloops.com>
2015-06-28 00:43:04 +0530

    Solution for HackerRank python arithmetic operators

---
+++ Python/arithmetic_operators.py
@@ -0,0 +1,13 @@
+# Solution for HackerRank python arithmetic operators  
+
+__autor__ = 'Saimadhu Polamuri'
+__website__ = 'www.dataaspirant.com'
+__createdon__ = '28-Jun-2015'
+
+number1 = int(raw_input())
+number2 = int(raw_input())
+
+print number1 + number2
+print number1 - number2
+print number1 * number2
+
