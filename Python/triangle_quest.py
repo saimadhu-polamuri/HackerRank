9842f586f1fbfa19b935636e9e8acc2edde64b19
sai <sai@team.quadloops.com>
2015-06-28 14:31:14 +0530

    Solution for HackerRank python triangle quest problem

---
+++ Python/triangle_quest.py
@@ -0,0 +1,8 @@
+# Solution for HackerRank python triangle quest
+
+__autor__ = 'Saimadhu Polamuri'
+__website__ = 'www.dataaspirant.com'
+__createdon__ = '28-Jun-2015'
+
+for i in range(1,input()):
+	print i * (10**i - 1) / 9
