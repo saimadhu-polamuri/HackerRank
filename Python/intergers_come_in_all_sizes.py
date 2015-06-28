6931c04605308eac3594d458c8c19879dee49991
sai <sai@team.quadloops.com>
2015-06-28 13:47:32 +0530

    intergers come in all sizes

---
+++ Python/intergers_come_in_all_sizes.py
@@ -0,0 +1,14 @@
+# Solution for HackerRank python intergers come in all sizes 
+
+__autor__ = 'Saimadhu Polamuri'
+__website__ = 'www.dataaspirant.com'
+__createdon__ = '28-Jun-2015'
+
+import math
+
+number1 = int(raw_input())
+number2 = int(raw_input())
+number3 = int(raw_input())
+number4 = int(raw_input())
+
+print pow(number1,number2) + pow(number3,number4)
