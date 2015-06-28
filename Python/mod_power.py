7fcda3b3046083d169bda1c42fc91492ba9e4f97
sai <sai@team.quadloops.com>
2015-06-28 13:32:22 +0530

    Solution for HackerRank python mod power problem

---
+++ Python/mod_power.py
@@ -0,0 +1,14 @@
+# Solution for HackerRank python mod power problem  
+
+__autor__ = 'Saimadhu Polamuri'
+__website__ = 'www.dataaspirant.com'
+__createdon__ = '28-Jun-2015'
+
+import math
+
+number1 = int(raw_input())
+number2 = int(raw_input())
+number3 = int(raw_input())
+
+print pow(number1,number2)
+print pow(number1,number2,number3)
