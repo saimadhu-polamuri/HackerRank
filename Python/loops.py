d2983885b1bd19d44908d6042e9c7a0c4e252d9c
sai <sai@team.quadloops.com>
2015-06-28 13:52:11 +0530

    Solution for HackerRank python intergers come in all sizes

---
+++ Python/loops.py
@@ -0,0 +1,11 @@
+# Solution for HackerRank python intergers come in all sizes 
+
+__autor__ = 'Saimadhu Polamuri'
+__website__ = 'www.dataaspirant.com'
+__createdon__ = '28-Jun-2015'
+
+import math
+number1 = int(raw_input())
+
+for i in xrange(0,number1):
+	print pow(i,2)
