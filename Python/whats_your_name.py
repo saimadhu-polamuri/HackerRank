5c3274064d0dbb692bb1c842ffae09d57ea62520
sai <sai@team.quadloops.com>
2015-06-28 13:58:37 +0530

    Solution for HackerRank python whats you name

---
+++ Python/whats_your_name.py
@@ -0,0 +1,11 @@
+# Solution for HackerRank python intergers come in all sizes 
+
+__autor__ = 'Saimadhu Polamuri'
+__website__ = 'www.dataaspirant.com'
+__createdon__ = '28-Jun-2015'
+
+firstName = raw_input()
+lastName = raw_input()
+
+if len(firstName) <= 10 and len(lastName) <= 10 :
+	print "Hello "+ firstName + " " + lastName + "! You just delved into python."
